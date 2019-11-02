4f260257c74634b9a9e3dac0ff131dfcb2415a85
MrLin <414048771@qq.com>
2019-11-02 20:55:27 +0800

    第14章 结构和其他数据形式

--- src/chapter14/booksave.c
+++ src/chapter14/booksave.c
@@ -1,11 +1,12 @@
 //#include <stdio.h>
+//#include <stdlib.h>
 //#include <string.h>
-//
-//char *s_gets(char *st, int n);
 //
 //#define MAXTITL 40
 //#define MAXAUTL 40
 //#define MAXBKS 100 /*书籍的最大数量*/
+//
+//char *s_gets(char *st, int n);
 //
 //struct book { /*建立book模板*/
 //
@@ -14,18 +15,43 @@
 //    float value;
 //};
 //
-//int main(void) {
-//    struct book library[MAXBKS]; /*book类型结构的数组*/
+//int main(void)
+//{
+//    struct book library[MAXBKS];/*结构数组*/
 //    int count = 0;
-//    int index;
+//    int index, filecount;
+//    FILE * pbooks;
+//    int size = sizeof(struct book);
 //
-//    printf("Please enter the book title.\n");
+//    if((pbooks = fopen("book.dat", "a+b")) == NULL)
+//    {
+//        fputs("Can't open book.dat file\n", stderr);
+//        exit(1);
+//    }
+//
+//    rewind(pbooks); /*定位到文件开始*/
+//    while (count < MAXBKS && fread(&library[count],size, 1,pbooks) ==1) {
+//        if(count == 0)
+//            puts("Current contents of book.dat:");
+//        printf("%s by %s: $%.2f\n", library[count].title,
+//                library[count].author, library[count].value);
+//        count++;
+//    }
+//
+//    filecount = count;
+//    if (count == MAXBKS)
+//    {
+//        fputs("The book.dat file is full.", stderr);
+//        exit(2);
+//    }
+//
+//    puts("Please add new book titles.");
 //    printf("Press [enter] at the start of line to stop.\n");
 //    while (count < MAXBKS && s_gets(library[count].title, MAXTITL) != NULL
 //           && library[count].title[0] != '\0') {
-//        printf("Now enter the author.\n");
+//        puts("Now enter the author.\n");
 //        s_gets(library[count].author, MAXAUTL);
-//        printf("Now enter the value.\n");
+//        puts("Now enter the value.\n");
 //        scanf("%f", &library[count++].value);
 //        while (getchar() != '\n')
 //            continue; /*清理输入行*/
@@ -38,17 +64,20 @@
 //        for (index = 0; index < count; index++)
 //            printf("%s by %s: $%.2f\n", library[index].title,
 //                   library[index].author, library[index].value);
+//        fwrite(&library[filecount], size, count - filecount, pbooks);
 //    }
 //
 //    else
 //        printf("No books? Too bad.\n");
 //
+//        puts("Bye.\n");
+//        fclose(pbooks);
+//
 //    return 0;
 //}
-//
 //char * s_gets(char *st, int n) {
-//    char *ret_val;
-//    char *find;
+//    char * ret_val;
+//    char * find;
 //
 //    ret_val = fgets(st, n, stdin);
 //    if (ret_val) {
