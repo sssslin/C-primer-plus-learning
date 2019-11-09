b8fe069807077ec21ade929a5f9da1c175ffde18
MrLin <414048771@qq.com>
2019-11-09 18:30:01 +0800

    第17章 高级数据表示

---
+++ src/chapter16/doubinc1.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//#include "names.h"
+//#include "names.h" // 不小心第二次包含头文件
+//#include "names_st.h"
+//
+//int main()
+//{
+//    names winner = {"Less", "Ismoor"};
+//    printf("The winner is %s %s.\n", winner.first, winner.last);
+//
+//    return 0;
+//}
---
+++ src/chapter16/glue.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//#define XNAME(n) x ## n
+//#define PRINT_XN(n) printf("x" #n " = %d\n", x ## n);
+//
+//int main(void)
+//{
+//    int XNAME(1) = 14; // 变成 int x1 = 14;
+//    int XNAME(2) = 20; // 变成 int x2 = 20;
+//
+//    int x3  = 30;
+//
+//    PRINT_XN(1); // 编程printf("x1 = %d\n", x1);
+//    PRINT_XN(2); // 编程printf("x2 = %d\n", x2);
+//    PRINT_XN(3); // 编程printf("x3 = %d\n", x3);
+//
+//    return 0;
+//}
